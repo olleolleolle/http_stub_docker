1bc2a8c96f8ac5d20762cc3794337e3d24e7505e
dueckes <matthew@ueckerman.net>
2017-01-31 14:24:05 +1100

    + AWS diagnostics

--- spec/bin/verify_docker_image_in_ecr.sh
+++ spec/bin/verify_docker_image_in_ecr.sh
@@ -5,6 +5,7 @@ set -e
 REPOSITORY_NAME=$1
 IMAGE_TAG=$2
 
+echo "Default Region: $AWS_DEFAULT_REGION"
 eval $(aws ecr get-login --region "$AWS_REGION")
 echo "Verifying repository $REPOSITORY_NAME has image with tag $IMAGE_TAG..."
 aws ecr describe-images --repository-name "$REPOSITORY_NAME" --image-ids imageTag="$IMAGE_TAG"
